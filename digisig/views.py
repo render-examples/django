220776fc8e2d42698538052477844d6119e977a2
John McEwan <jmcewan3@yahoo.ca>
2024-09-05 06:17:16 -0500

    updates

--- digisig/views.py
+++ digisig/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django.urls import reverse
+from datetime import datetime
+from time import time
+
+from .models import *
+
+
 
 # Create your views here.
+def index(request):
+    # return render(request, 'sealquery/index.html', {})
+
+    starttime = time()
+
+    pagetitle = 'title'
+    template = loader.get_template('digisig/index.html')
+
+    #### update this to remove databasecall
+
+    manifestation_total = Manifestation.objects.count()
+    seal_total = Seal.objects.count()
+    item_total = Manifestation.objects.distinct('fk_support__fk_part__fk_item').count()
+    catalogue_total = Sealdescription.objects.count()
+
+    context = {
+        'pagetitle': pagetitle,
+        'manifestation_total': manifestation_total,
+        'seal_total': seal_total,
+        'item_total': item_total,
+        'catalogue_total': catalogue_total,
+        }
+
+    print("Compute Time:", time()-starttime)
+    return HttpResponse(template.render(context, request))
--- sealquery/urls.py
+++ sealquery/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('render.urls')),
+    #path('', include('render.urls')),
+    path('', include('digisig.urls')),
 ]
