44d6d47200eeb65706ccb1c79dd207dd9cdec8ba
John McEwan <jmcewan3@yahoo.ca>
2024-09-30 13:12:44 -0500

    updates

--- sealquery/settings.py
+++ sealquery/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.digisig.org", "digisig.org", "127.0.0.1", ".localhost"]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
