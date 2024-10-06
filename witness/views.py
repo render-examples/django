d95a3a65fef141df4787523479f23cad8d54558b
John McEwan <jmcewan3@yahoo.ca>
2024-10-06 05:51:13 -0500

    updates

--- witness/views.py
+++ witness/views.py
@@ -63,7 +63,7 @@ def graph(request):
     # mapdic["features"] = placelist
 
 
-    graphdata = {"nodes": [{"id":"id1","name":"name1","val": 1},{"id":"id2","name":"name2","val": 10},],"links":[{"source":"id1","target":"id2"},]}
+    graphdata = {nodes: [{"id":"id1","name":"name1","val": 1},{"id":"id2","name":"name2","val": 10}],links:[{"source":"id1","target":"id2"}]}
 
     template = loader.get_template('witness/graph.html')
     context = {'graphdata': graphdata}
