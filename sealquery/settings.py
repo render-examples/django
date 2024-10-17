763645398883dc010d83c8451f9a8f5ec99e8fe7
John McEwan <jmcewan3@yahoo.ca>
2024-10-16 20:25:46 -0500

    updates

--- sealquery/settings.py
+++ sealquery/settings.py
@@ -42,14 +42,14 @@ INTERNAL_IPS = ["127.0.0.1"]
 
 INSTALLED_APPS = [
     # 'render.apps.RenderConfig',
-    'digisig.apps.DigisigConfig',
-    'witness.apps.WitnessConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'digisig.apps.DigisigConfig',
+    'witness.apps.WitnessConfig',
     'debug_toolbar',
 ]
 
