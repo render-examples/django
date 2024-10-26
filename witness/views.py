9e4ffdac2093b1613cd84feb20d650d45ae1a037
John McEwan <jmcewan3@yahoo.ca>
2024-10-25 22:13:41 -0500

    Update views.py

--- witness/views.py
+++ witness/views.py
@@ -125,23 +125,23 @@ def search(request, searchtype):
 
         londonevents = Location.objects.filter(fk_region=87).values('locationname__locationreference__fk_event')
 
-        print (len(londonevents))
+        # print (len(londonevents))
 
         individual_object = individualsearch()
 
-        print (len(individual_object))
+        # print (len(individual_object))
 
         individual_set1 = individual_object.exclude(
             id_individual=10000019).filter(
             fk_individual_event__in=londonevents)
 
-        print (len(individual_set1))        
+        # print (len(individual_set1))        
 
         individual_object = individual_object.exclude(
             id_individual=10000019).filter(
             fk_individual_event__in=londonevents).distinct('id_individual')
 
-        print (len(individual_object))
+        # print (len(individual_object))
 
         if request.method == "POST":
             form = PeopleForm(request.POST)
@@ -224,7 +224,17 @@ def person_page(request, witness_entity_number):
     reference_set = referenceset_references(individual_object, reference_set)
 
     # parish where active
-    parishactive = reference_set.annotate(countfk_event__)
+    parishstats = {}
+
+    for r in reference_set.values():
+        print (r['location_id'])
+        parishvalue = r['location']
+        if parishvalue in parishstats:
+            parishstats[parishvalue] += 1
+        else:
+            parishstats[parishvalue] = 1
+
+    print (parishstats)
 
     context = {
         'pagetitle': pagetitle,
