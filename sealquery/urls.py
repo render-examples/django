800252a984fffa9d0497ad695bd430ae3a56eb52
John McEwan <jmcewan3@yahoo.ca>
2024-10-29 12:42:22 -0500

    updates

--- sealquery/urls.py
+++ sealquery/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', include('render.urls')),
     #path('', include('digisig.urls')),
-    path('', include('witness.urls')), 
+    # path('', include('witness.urls')), 
 ]
