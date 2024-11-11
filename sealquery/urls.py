4039e298718525ceae8278c930ccfd1d84b5e305
John McEwan <jmcewan3@yahoo.ca>
2024-11-11 13:51:32 -0600

    updates

--- sealquery/urls.py
+++ sealquery/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', include('render.urls')),
-    path('', include('digisig.urls')),
+    #path('', include('digisig.urls')),
     #path('', include('witness.urls')), 
 ]
